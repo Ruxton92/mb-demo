518669f68a93d2095956c44dfc1644476e154bae
Artemiy Kondakov <artkon92@gmail.com>
2015-08-25 16:28:45 +0300

    [MNR-2] Carousel with progressbar

--- src/index/view.js
+++ src/index/view.js
@@ -1,7 +1,36 @@
+import $ from 'jquery';
 import {ItemView} from 'backbone.marionette';
 import template from './template.hbs';
 
 export default ItemView.extend({
   template: template,
-  className: 'index'
+  className: 'index',
+
+  ui: {
+  	'carousel': '.mb-carousel',
+  	'progress': '.progress-bar',
+  },
+
+  events: {
+    "slide.bs.carousel @ui.carousel": "slideStart",
+    "slid.bs.carousel @ui.carousel": "slideEnd",
+    "mouseenter @ui.carousel": 'pauseStart',
+    "mouseleave @ui.carousel": 'pauseEnd',
+  },
+
+  onRender() {
+  	this.ui.carousel.carousel({
+	    interval: 5600,
+	    pause: false
+		});
+    this.ui.carousel.addClass('active');
+  },
+
+  slideStart(e) {
+    this.ui.carousel.removeClass('active');
+  },
+
+  slideEnd(e) {
+    this.ui.carousel.addClass('active');
+  },
 });

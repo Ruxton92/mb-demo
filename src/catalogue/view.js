941f3a2f25019be6145927e8fef081ef0187831f
Artemiy Kondakov <artkon92@gmail.com>
2015-09-07 18:01:17 +0300

    [MNR-25] Callback modal

--- src/catalogue/view.js
+++ src/catalogue/view.js
@@ -1,7 +1,22 @@
 import {ItemView} from 'backbone.marionette';
 import template from './template.hbs';
 
+import ModalService from '../modal/service';
+
+import CallbackModalView from '../modal/callback/view';
+
 export default ItemView.extend({
   template: template,
-  className: 'mb-catalogue-wrapper'
+  className: 'mb-catalogue-wrapper',
+
+  events: {
+  	'click .js-call-callback-modal': 'showCallbackModal'
+  },
+
+  showCallbackModal(e) {
+    e.preventDefault();
+    var view = new CallbackModalView();
+    ModalService.request('open', view);
+  }
+
 });

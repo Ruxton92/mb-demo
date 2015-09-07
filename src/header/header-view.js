9f9ae102a4a175478d08b96b03efc62f01a4abef
Artemiy Kondakov <artkon92@gmail.com>
2015-09-07 12:09:52 +0300

    [MNR-21] Empty modal in header

--- src/header/header-view.js
+++ src/header/header-view.js
@@ -1,6 +1,19 @@
 import {ItemView} from 'backbone.marionette';
 import template from './header.hbs';
 
+import ModalService from '../modal/service';
+
+import StoreInfoView from '../footer/store_info/store-info-view';
+
 export default ItemView.extend({
-  template: template
+  template: template,
+  events: {
+    "click .js-store-info": "storeInfo"
+  },
+
+  storeInfo(e) {
+    e.preventDefault();
+    var view = new StoreInfoView();
+    ModalService.request('open', view);
+  }
 });

d0c05bd5118a29b42213e467ba645a8c31ae118f
Mikhail Gordeyev <m.gordeyev@gmail.com>
2015-09-08 13:32:17 +0300

    temp commit for support modal

--- src/catalogue/view.js
+++ src/catalogue/view.js
@@ -6,6 +6,7 @@ import ModalService from '../modal/service';
 
 import CallbackModalView from '../modal/callback/view';
 import EmailModalView from '../modal/email/view';
+import SupportModalView from '../modal/support/view';
 
 export default ItemView.extend({
   template: template,
@@ -14,7 +15,12 @@ export default ItemView.extend({
   events: {
     'click .js-call-callback-modal': 'showCallbackModal',
     'click .js-call-email-modal': 'showEmailModal',
+    'click .js-support-modal': 'showSupportModal',
     'click .js-tab': 'switchTab'
+  },
+
+  onShow() {
+    this.$('.js-support-modal').click();
   },
 
   showCallbackModal(e) {
@@ -26,6 +32,12 @@ export default ItemView.extend({
   showEmailModal(e) {
     e.preventDefault();
     var view = new EmailModalView();
+    ModalService.request('open', view);
+  },
+
+  showSupportModal(e) {
+    e.preventDefault();
+    var view = new SupportModalView();
     ModalService.request('open', view);
   },
 

60fbd0d2cb430ccf5e53553cb1e0642bd2e6a95a
omjan <anton.s.pilipenko@gmail.com>
2015-09-23 11:05:26 +0300

    убрал jsonnp

--- src/product_detail/model.js
+++ src/product_detail/model.js
@@ -7,17 +7,17 @@ export default Backbone.Model.extend({
     // console.log(response);
     return jsonp(response);
   },
-  sync(method, model, options) {
-    // By setting the dataType to "jsonp", jQuery creates a function
-    // and adds it as a callback parameter to the request, e.g.:
-    // [url]&callback=jQuery19104472605645155031_1373700330157&q=bananarama
-    // If you want another name for the callback, also specify the
-    // jsonpCallback option.
-    // After this function is called (by the JSONP response), the script tag
-    // is removed and the parse method is called, just as it would be
-    // when AJAX was used.
-    options.jsonpCallback = 'fake';
-    options.dataType = "jsonp";
-    return Backbone.sync(method, model, options);
+  // sync(method, model, options) {
+  //   // By setting the dataType to "jsonp", jQuery creates a function
+  //   // and adds it as a callback parameter to the request, e.g.:
+  //   // [url]&callback=jQuery19104472605645155031_1373700330157&q=bananarama
+  //   // If you want another name for the callback, also specify the
+  //   // jsonpCallback option.
+  //   // After this function is called (by the JSONP response), the script tag
+  //   // is removed and the parse method is called, just as it would be
+  //   // when AJAX was used.
+  //   options.jsonpCallback = 'fake';
+  //   options.dataType = "jsonp";
+  //   return Backbone.sync(method, model, options);
   },
 });

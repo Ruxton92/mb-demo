757a0bd9d57bf01d158fdbbe2199e6bf4ea9cf4d
Artemiy Kondakov <artkon92@gmail.com>
2015-10-14 14:30:42 +0300

    [MNR-87][MNR-88][MNR-89][MNR-90][MNR-91][MNR-92][MNR-93] Storeinfo page

--- src/store_info/view.js
+++ src/store_info/view.js
@@ -11,12 +11,26 @@ export default LayoutView.extend({
   },
 
   ui: {
+    'tabLinks': '.mb-nav-inside a',
+    'tabs': '[class^=tab]'
   },
 
   events: {
+    'click @ui.tabLinks': 'tabLinkClick'
   },
 
   onShow() {
+    this.ui.tabs.addClass('hide');
+    this.ui.tabs.eq(0).removeClass('hide');
+  },
+
+  tabLinkClick(e) {
+    e.preventDefault();
+    this.ui.tabs.addClass('hide');
+    this.ui.tabLinks.removeClass('active');
+    $(e.currentTarget).addClass('active');
+    let tabName = $(e.currentTarget).data('tab');
+    $('.' + tabName).removeClass('hide');
   }
 
 });

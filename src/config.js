bda6091522a8885767eac018186eb53c0c0df8f6
Ubayd Rakhmonzoda <ubaidinoyatov@gmail.com>
2015-10-28 20:32:50 +0300

    [MNR-139] Fix detail - breakpoints improvenets

--- src/config.js
+++ src/config.js
@@ -3,9 +3,9 @@ let config = {api: {}};
 let production = false;
 
 if (production) {
-  config.api.url = '//sos-dev.nolteundlauth.de';
-} else{
   config.api.url = '//mbos-dev.nolteundlauth.de';
+} else{
+  config.api.url = '//sos-dev.nolteundlauth.de';
 }
 
 module.exports = config;

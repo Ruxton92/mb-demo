5d6c3172c35043fc05c9a78e505bde1b0fc940b9
Ubayd Rakhmonzoda <ubaidinoyatov@gmail.com>
2015-10-30 16:47:31 +0300

    Watchlist markup

--- src/watchlist/view.js
+++ src/watchlist/view.js
@@ -2,6 +2,11 @@ import $ from 'jquery';
 import {LayoutView} from 'backbone.marionette';
 import template from './template.hbs';
 
+import ModalService from '../modal/service';
+
+import CallbackModalView from '../modal/callback/view';
+import EmailModalView from '../modal/email/view';
+import SupportModalView from '../modal/support/view';
 
 export default LayoutView.extend({
   template: template,
@@ -10,13 +15,38 @@ export default LayoutView.extend({
   regions: {
   },
 
-  ui: {
-  },
-
   events: {
+    'click .js-call-callback-modal': 'showCallbackModal',
+    'click .js-call-email-modal': 'showEmailModal',
+    'click .js-support-modal': 'showSupportModal',
+    'click .js-tab': 'switchTab'
   },
 
   onShow() {
+  },
+
+  showCallbackModal(e) {
+    e.preventDefault();
+    var view = new CallbackModalView();
+    ModalService.request('open', view);
+  },
+
+  showEmailModal(e) {
+    e.preventDefault();
+    var view = new EmailModalView();
+    ModalService.request('open', view);
+  },
+
+  showSupportModal(e) {
+    e.preventDefault();
+    var view = new SupportModalView();
+    ModalService.request('open', view);
+  },
+
+  switchTab(e) {
+    e.preventDefault();
+    this.$('.js-tab').removeClass('active');
+    $(e.currentTarget).addClass('active');
   }
 
 });

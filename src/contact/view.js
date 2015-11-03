9e9f4e6c5d143469882858d2a8ebf345ae5cfc09
Ubayd Rakhmonzoda <ubaidinoyatov@gmail.com>
2015-11-03 19:40:37 +0300

    Callback Dialog - fix datepicker

--- src/contact/view.js
+++ src/contact/view.js
@@ -1,7 +1,35 @@
 import {ItemView} from 'backbone.marionette';
 import template from './template.hbs';
+import ModalService from '../modal/service';
+import CallbackModalView from '../modal/callback/view';
+import EmailModalView from '../modal/email/view';
+import SupportModalView from '../modal/support/view';
 
 export default ItemView.extend({
   template: template,
-  className: 'contact-wrapper'
+  className: 'contact-wrapper',
+
+  events: {
+    'click .js-call-callback-modal': 'showCallbackModal',
+    'click .js-call-email-modal': 'showEmailModal',
+    'click .js-support-modal': 'showSupportModal'
+  },
+  showCallbackModal(e) {
+    e.preventDefault();
+    var view = new CallbackModalView();
+    ModalService.request('open', view);
+  },
+
+  showEmailModal(e) {
+    e.preventDefault();
+    var view = new EmailModalView();
+    ModalService.request('open', view);
+  },
+
+  showSupportModal(e) {
+    e.preventDefault();
+    var view = new SupportModalView();
+    ModalService.request('open', view);
+  }
+
 });

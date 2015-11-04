4104a4e4da5627def390b4e0667d059933f2b63c
Ubayd Rakhmonzoda <ubaidinoyatov@gmail.com>
2015-11-04 15:34:02 +0300

    link contact dialogs

--- src/modal/support/view.js
+++ src/modal/support/view.js
@@ -6,6 +6,14 @@ export default ItemView.extend({
   template: template,
   className: 'mb-fullscreen-modal-content',
 
+  ui: {
+    'deliveryTypes': '.delivery-type',
+  },
+
+  events: {
+    'change [name="delivery-type"]': 'changeDeliveryType'
+  },
+
   triggers: {
     'click .btn-default' : 'cancel',
     'click .close'       : 'cancel',
@@ -18,5 +26,10 @@ export default ItemView.extend({
         minDate: moment().format(),
         daysOfWeekDisabled: [6]
     });
+  },
+  changeDeliveryType(e) {
+    e.preventDefault();
+    this.ui.deliveryTypes.addClass('hide');
+    this.$el.find(`.${$(e.currentTarget).val()}`).removeClass('hide');
   }
 });

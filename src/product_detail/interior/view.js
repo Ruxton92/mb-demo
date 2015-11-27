691718bfb7d1969462f8e223954751c9b42635e2
Ubayd Rakhmonzoda <ubaidinoyatov@gmail.com>
2015-11-27 20:06:33 +0300

    [MNR-164] - OfferDetailPage hotspots

--- src/product_detail/interior/view.js
+++ src/product_detail/interior/view.js
@@ -2,11 +2,11 @@ import $ from 'jquery';
 import Backbone from 'backbone';
 import {CompositeView} from 'backbone.marionette';
 import {ItemView} from 'backbone.marionette';
+
 import template from './template.hbs';
-import itemTemplate from './slide_template.hbs';
+import itemTemplate from './item_template.hbs';
 
 import ModalService from '../../modal/service';
-
 import InteriorModalView from '../../modal/interior/view';
 
 
@@ -30,54 +30,34 @@ export default CompositeView.extend({
   template: template,
   className: 'mb-model-detail-interior-block row',
   childView: SlideView,
-  childViewContainer: '.mb-interior-large-photo-block, .carousel-inner',
+  childViewContainer: '.mb-interior-large-photo-block',
 
   ui: {
-    'carousel': '.mb-carousel',
-    'switchLight': '.js-switch-light',
-    'openModal': '.js-interior-modal',
-    'slides': '.mb-carousel .item',
-    'progress': '.progress-bar',
+    'openModal': '.js-interior-modal'
   },
 
   events: {
-    'click @ui.openModal': 'showInteriorModal',
+    'click @ui.openModal': 'showInteriorModal'
   },
 
   initialize() {
   },
 
   onShow() {
-    this.ui.carousel.find('.mb-interior-large-photo').slice(0,2).addClass('active');
-    this.ui.carousel.carousel({
-      interval: 2000,
-      
-    }).carousel('pause');
-    this.ui.carousel.find('.item:first').addClass('active');
-    this.slidesNum = this.collection.length;
-    this.interval = parseFloat(100 / this.slidesNum);
-    this.ui.progress.css('width', this.interval + '%');
-    this.currentSlide = 1;
-    this.ui.carousel.addClass('active');
+    
   },
 
   showInteriorModal(e) {
     e.preventDefault();
-    let view = new InteriorModalView({collection: this.collection});
+    let intDay = this.model.get('stageModules')[0].data[0].car.images360IntDayClosed;
+    let intNight = this.model.get('stageModules')[0].data[0].car.images360IntNightClosed;
+    let slidesInt = [];
+    for (let i = 0; i < intDay.length; i++) {
+      slidesInt.push({day: intDay[i].md.url, night: 'http://placehold.it/1305x734'});
+    }
+    this.interiorSlidesCollection = new Backbone.Collection(slidesInt);
+    let view = new InteriorModalView({collection: this.interiorSlidesCollection});
     ModalService.request('open', view);
-  },
-
-  slideStart(e) {
-    if(e.direction == 'left') {
-      if (this.currentSlide == this.slidesNum) this.currentSlide = 1;
-      else this.currentSlide += 1;
-    }
-    else {
-      if (this.currentSlide == 1) this.currentSlide = this.slidesNum;
-      else this.currentSlide -= 1;
-    }
-    this.ui.progress.css('width', this.interval * (this.currentSlide) + '%');
-  },
-
+  }
 });
 
